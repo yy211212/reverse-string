167b5129f760de9eae34e73554e5d5ef1ed340a6
yy211212 <limyuyoung211212@gmail.com>
2024-12-05 15:51:34 +0800

    commit

---
+++ main.py
@@ -0,0 +1,6 @@
+string=input("Please enter your own string :")
+string2 = ('')
+for i in string:
+    string2 = i+string2
+print("\nThe Original String =", string)
+print("The Reversed String =", string2)
